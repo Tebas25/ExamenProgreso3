fa91b9c4ba0d85c91deecc679fd04b170cf802dd
Andres Sebastian Almeida Yanez <andres.almeida.yanez@udla.edu.ec>
2025-01-20 08:23:51 -0500

    Creacion final de la API

--- ExamenProgreso3/Models/Pais.cs
+++ ExamenProgreso3/Models/Pais.cs
@@ -8,8 +8,37 @@ namespace ExamenProgreso3.Models
 {
     public class Pais
     {
-        public string Name { get; set; }
-        public string Region { get; set; }
-        public string Maps { get; set; }
+
+        public class Class1
+        {
+            public Name name { get; set; }
+            public string region { get; set; }
+            public Maps maps { get; set; }
+        }
+
+        public class Name
+        {
+            public string common { get; set; }
+            public string official { get; set; }
+            public Nativename nativeName { get; set; }
+        }
+
+        public class Nativename
+        {
+            public Spa spa { get; set; }
+        }
+
+        public class Spa
+        {
+            public string official { get; set; }
+            public string common { get; set; }
+        }
+
+        public class Maps
+        {
+            public string googleMaps { get; set; }
+            public string openStreetMaps { get; set; }
+        }
+
     }
 }
--- ExamenProgreso3/Repositories/PaisAPIRepository.cs
+++ ExamenProgreso3/Repositories/PaisAPIRepository.cs
@@ -1,14 +1,32 @@
 ﻿using ExamenProgreso3.Interfaces;
 using ExamenProgreso3.Models;
+using Microsoft.Extensions.Logging.Abstractions;
+using Newtonsoft.Json;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
 
 namespace ExamenProgreso3.Repositories
 {
     public class PaisAPIRepository : IPaisRepositoy
     {
-        public Task<Pais> Pais(string nombrePais)
+        public async Task<Pais> Pais(string nombrePais)
         {
+            var pais = new Pais();
+            var client = new HttpClient();
             string url = "https://restcountries.com/v3.1/name/" + nombrePais + "?fields=name,region,maps";
-
+            client.BaseAddress = new Uri(url);
+            HttpResponseMessage respuesta = await client.GetAsync(url);
+            if (respuesta.IsSuccessStatusCode)
+            {
+                var responseBody = await respuesta.Content.ReadAsStringAsync();
+                JsonNode node = JsonNode.Parse(responseBody);
+                JsonNode resultado = JsonNode.Parse(responseBody);
+                pais = JsonConvert.DeserializeObject<Pais>(resultado.ToString());
+                return pais;
+            }
+            return null;
+            
         }
     }
 }
